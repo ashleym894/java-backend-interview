167250ff90827714c4cb944a019887d966c38ed2
ashleym894 <ashleyren894@outlook.com>
2025-10-02 19:15:00 -0400

    Add files via upload

---
+++ src/main/java/com/synchrony/messaging/EventPublisher.java
@@ -0,0 +1,19 @@
+package com.synchrony.messaging;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventPublisher {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public EventPublisher(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void publishImageEvent(String username, String imageName) {
+        String message = "User: " + username + " uploaded image: " + imageName;
+        kafkaTemplate.send("image-events", username, message);
+    }
+}
